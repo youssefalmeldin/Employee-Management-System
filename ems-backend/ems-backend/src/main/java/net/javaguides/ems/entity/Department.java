131298cd02f233fadd056e5fedbf73670a72943f
Youssef Alm El Din <76973497+youssefalmeldin@users.noreply.github.com>
2024-11-28 23:28:37 +0200

    Add files via upload

---
+++ ems-backend/ems-backend/src/main/java/net/javaguides/ems/entity/Department.java
@@ -0,0 +1,30 @@
+package net.javaguides.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String departmentDescription;
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Employee> employees;
+}
